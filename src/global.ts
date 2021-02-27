b890698775b79cb16e51f20804e489354bcf4720
baian1 <chizhongnan1@gmail.com>
2021-02-28 01:35:06 +0800

    添加柱子

--- src/global.ts
+++ src/global.ts
@@ -45,7 +45,6 @@ function main() {
   let renderRequested = false;
   function render() {
     renderRequested = false;
-
     if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
       camera.aspect = canvas.clientWidth / canvas.clientHeight;
@@ -66,6 +65,108 @@ function main() {
 
   controls.addEventListener("change", requestRenderIfNotRequested);
   window.addEventListener("resize", requestRenderIfNotRequested);
+
+  async function loadFile(url: string): any {
+    const req = await fetch(url);
+    return req.text();
+  }
+
+  function parseData(text: string) {
+    const data: (number | undefined)[][] = [];
+    const settings = { data };
+    let max: number;
+    let min: number;
+    // split into lines
+    text.split("\n").forEach((line) => {
+      // split the line by whitespace
+      const parts = line.trim().split(/\s+/);
+      if (parts.length === 2) {
+        // only 2 parts, must be a key/value pair
+        settings[parts[0]] = parseFloat(parts[1]);
+      } else if (parts.length > 2) {
+        // more than 2 parts, must be data
+        const values = parts.map((v) => {
+          const value = parseFloat(v);
+          if (value === settings.NODATA_value) {
+            return undefined;
+          }
+          max = Math.max(max === undefined ? value : max, value);
+          min = Math.min(min === undefined ? value : min, value);
+          return value;
+        });
+        data.push(values);
+      }
+    });
+    console.log(settings);
+    return Object.assign(settings, { min, max });
+  }
+
+  function addBoxes(file: {
+    data: (number | undefined)[][];
+    min: number;
+    max: number;
+  }) {
+    const { min, max, data } = file;
+    const range = max - min;
+
+    // make one box geometry
+    const boxWidth = 1;
+    const boxHeight = 1;
+    const boxDepth = 1;
+    const geometry = new THREE.BoxBufferGeometry(boxWidth, boxHeight, boxDepth);
+    // make it so it scales away from the positive Z axis
+    geometry.applyMatrix4(new THREE.Matrix4().makeTranslation(0, 0, 0.5));
+
+    // these helpers will make it easy to position the boxes
+    // We can rotate the lon helper on its Y axis to the longitude
+    const lonHelper = new THREE.Object3D();
+    scene.add(lonHelper);
+    // We rotate the latHelper on its X axis to the latitude
+    const latHelper = new THREE.Object3D();
+    lonHelper.add(latHelper);
+    // The position helper moves the object to the edge of the sphere
+    const positionHelper = new THREE.Object3D();
+    positionHelper.position.z = 1;
+    latHelper.add(positionHelper);
+
+    const lonFudge = Math.PI * 0.5;
+    const latFudge = Math.PI * -0.135;
+    data.forEach((row, latNdx) => {
+      row.forEach((value, lonNdx) => {
+        if (value === undefined) {
+          return;
+        }
+        const amount = (value - min) / range;
+        const material = new THREE.MeshBasicMaterial();
+        const hue = THREE.MathUtils.lerp(0.7, 0.3, amount);
+        const saturation = 1;
+        const lightness = THREE.MathUtils.lerp(0.4, 1.0, amount);
+        material.color.setHSL(hue, saturation, lightness);
+        const mesh = new THREE.Mesh(geometry, material);
+        scene.add(mesh);
+
+        // adjust the helpers to point to the latitude and longitude
+        lonHelper.rotation.y =
+          THREE.MathUtils.degToRad(lonNdx + file.xllcorner) + lonFudge;
+        latHelper.rotation.x =
+          THREE.MathUtils.degToRad(latNdx + file.yllcorner) + latFudge;
+
+        // use the world matrix of the position helper to
+        // position this mesh.
+        positionHelper.updateWorldMatrix(true, false);
+        mesh.applyMatrix4(positionHelper.matrixWorld);
+
+        mesh.scale.set(0.005, 0.005, THREE.MathUtils.lerp(0.01, 0.5, amount));
+      });
+    });
+  }
+
+  loadFile(
+    "https://threejsfundamentals.org/threejs/resources/data/gpw/gpw_v4_basic_demographic_characteristics_rev10_a000_014mt_2010_cntm_1_deg.asc"
+  )
+    .then(parseData)
+    .then(addBoxes)
+    .then(render);
 }
 
 main();
